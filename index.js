c2d58782011abaf08812a7bf274d79988922cac1
Harri Nieminen <harrinieminenkoodaargmail.com>
2023-03-06 14:32:46 +0200

    Add new get

--- index.js
+++ index.js
@@ -16,12 +16,15 @@ const requestLogger = (request, response, next) => {
 
 
 app.use(cors())
-app.use(express.static('/build'))
 app.use(express.json())
 app.use(requestLogger)
+app.use(express.static('build'))
 
 
 //add comment
+app.get("*", (_, res) => // this solves the issue of individual routes /profile/test not working after deployment
+  res.sendFile(path.resolve("build", "index.html"))
+)
 
 /**
  * Get all recipes
